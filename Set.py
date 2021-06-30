1d3bd5cea3f5e9e08cb00795c55233b1415aded7
Ishika Kulkarni <44474766+Ishika13@users.noreply.github.com>
2021-06-30 18:53:43 +0530

    File added

---
+++ Set.py
@@ -0,0 +1,10 @@
+#Apply your knowledge of the .add() operation to help your friend Rupal.
+#Rupal has a huge collection of country stamps. 
+#She decided to count the total number of distinct country stamps in her collection. 
+#She asked for your help. You pick the stamps one by one from a stack of n country stamps.
+
+n = int(input())
+s = set()
+for i in range (n):
+    s.add(input())
+print (len(s))  
